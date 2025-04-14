5092f06075c9df2aeea331ab93c032203ee97971
Alex Ayibyan <alex.ayibyan@student.ap.be>
2025-04-14 15:22:25 +0200

    Rebuild app because of gradle issue

--- rent_app/lib/main.dart
+++ rent_app/lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:rent_app/firebase_options.dart';
 import 'package:rent_app/login_screen.dart';
 
@@ -8,11 +8,12 @@ Future<void> main() async {
   await Firebase.initializeApp(
   options: DefaultFirebaseOptions.currentPlatform,
 );
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
